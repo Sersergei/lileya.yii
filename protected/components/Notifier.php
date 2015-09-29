a933153ac2d1180c5debf891ff315f67bb8228de
Sergei <seresrgei@bigmir.net>
2015-09-29 09:35:54 +0300

    добавлено письмо администратору

--- protected/components/Notifier.php
+++ protected/components/Notifier.php
@@ -11,6 +11,9 @@ class Notifier{
     Ваш заказ №".$event-> zakaz->id." принят в обработку.
     В течение дня Мы с Вами свяжемся и договоримся о способе доставки товара.".$event->user->flash;
     mail($event->user->email,"Новый заказ ",$text);
+        $text="В твоем магазине новый заказ№".$event-> zakaz->id.
+            "Заказал".$event->user->usernam."вяжись с ним по тедефону".$event->user->phone;
+      mail(kuzmenko.kseniya@gmail.com,"Новый заказ ",$text);
     }
 
 
