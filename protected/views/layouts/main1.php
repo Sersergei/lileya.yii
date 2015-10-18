4496ebb2aed16a6edef2a35f6469a40e51a68f06
Sergei <sersergei@bigmir.net>
2015-10-18 21:52:11 +0300

    livechat

--- protected/views/layouts/main1.php
+++ protected/views/layouts/main1.php
@@ -219,6 +219,10 @@
     <div id ="footer"><p class="par">Copyright © 2014 Sergey Ohonko & Kseniya Kuzmenko</div>
 
 </div>
-
+ <!-- BEGIN JIVOSITE CODE {literal} -->
+ <script type='text/javascript'>
+     (function(){ var widget_id = 'NzMgcXwmPY';
+         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+ <!-- {/literal} END JIVOSITE CODE -->
 </body>
 </html>
